c1ab381dc0850b96d163e0bdef4f66fd844e13a4
Nic Cope <negz@rk0n.org>
2021-07-09 18:17:02 +0000

    Add initial migrating from terraform HCL
    Signed-off-by: Nic Cope <negz@rk0n.org>

---
+++ migrating-from-terraform/tf/main.tf
@@ -0,0 +1,26 @@
+variable "location" {
+  type    = string
+  default = "US"
+}
+
+output "url" {
+  value = google_storage_bucket.example.self_link
+}
+
+resource "google_storage_bucket" "example" {
+  name     = "crossplane-example-${terraform.workspace}"
+  location = var.location
+}
+
+provider "google" {
+  credentials = "gcp-credentials.json"
+  project     = "crossplane-playground"
+}
+
+terraform {
+  backend "gcs" {
+    bucket      = "crossplane-example-tf"
+    prefix      = "provider-terraform/default"
+    credentials = "gcp-credentials.json"
+  }
+}
